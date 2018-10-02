64680aca5548396cfbed2bf4d50eef4e9441aa80
sasukeamjed <sasukeamjed@gmail.com>
2018-10-02 15:45:26 +0400

    list view builder working fine

--- flutter_app/lib/home.dart
+++ flutter_app/lib/home.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-
+class HomeScreen extends StatefulWidget {
   String username;
 
   HomeScreen(this.username);
 
   @override
+  HomeScreenState createState() {
+    return new HomeScreenState();
+  }
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  var editMode = false;
+  var noteController = TextEditingController();
+
+  var notes = new List();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('${widget.username}'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+            ),
+            onPressed: () {
+              setState(() {
+                editMode = !editMode;
+                if (editMode == false) {
+                  if (noteController.text.isEmpty) {
+                    print("there is nothing to add");
+                    return null;
+                  } else {
+                    notes.add(noteController.text);
+                    noteController.clear();
+                    print(notes);
+                  }
+                }
+                print(editMode);
+              });
+            },
+          ),
+        ],
       ),
       body: Center(
-        child: Text('$username'),
+        child: editMode
+            ? TextField(
+                controller: noteController,
+                autofocus: true,
+              )
+            : listNotesBuilder(),
       ),
     );
+  }
+
+  Widget listNotesBuilder() {
+    Widget list = Text('There is not items available');
+
+    if (notes.isEmpty) {
+      return list;
+    } else {
+      list = ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(notes[index]),
+          );
+        },
+      );
+    }
+    return list;
   }
 }
